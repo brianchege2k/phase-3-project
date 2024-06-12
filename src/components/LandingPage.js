7425ffc2d2b75b61bca98424419026e3dadc161d
brianchege2k <chegebrian84@gmail.com>
2024-06-12 13:08:46 +0300

    carousel added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+import './LandingPage.css';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
 
 const LandingPage = () => {
   return (
-    <div className="p-4">
-      <h1 className='hero-text'>Welcome to Recipe Central</h1>
-      <p>Your favorite place to discover, manage and share recipes.</p>
-    </div>
+    <Container className="mt-4">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {img1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h1>Welcome to Recipe Central</h1>
+            <p>Your favorite place to create, manage and share recipes.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h1>Discover New Recipes</h1>
+            <p>Explore a variety of recipes from around the world.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {img3}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h1>Create Your Own</h1>
+            <p>Share your unique recipes with the community.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 };
 
